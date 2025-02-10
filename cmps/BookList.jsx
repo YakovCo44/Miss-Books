2b496248da336691082f15e74d1828f7b5f09373
Yakov Cohen <yakovco44@gmail.com>
2025-02-10 12:03:40 +0200

    Added delete confirmation

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -1,6 +1,11 @@
 import { BookPreview } from "./BookPreview"
 
 export function BookList({ books, onDelete, onEdit }) {
+    function confirmDelete(bookId) {
+        const isConfirmed = window.confirm("Are you sure you want to delete this book?")
+        if (isConfirmed) onDelete(bookId)
+    }
+
     return (
         <ul className="book-list">
             {books.map(book => (
@@ -8,7 +13,7 @@ export function BookList({ books, onDelete, onEdit }) {
                     <BookPreview book={book} />
                     <div className="book-actions">
                         <button onClick={() => onEdit(book)}>Edit</button>
-                        <button onClick={() => onDelete(book.id)}>Delete</button>
+                        <button onClick={() => confirmDelete(book.id)}>Delete</button>
                     </div>
                 </li>
             ))}
